641c5146d983cbeb79edeb8f5d1ef6702d0b58db
Mohammed Shafeek <mail2shafeek.sa@gmai.com>
2024-01-04 04:25:35 +0400

    Add nav bar and color shift

--- src/helpers.jsx
+++ src/helpers.jsx
@@ -12,27 +12,41 @@ export function startHelloScreen() {
 export function hideHelloScreen(){
     const helloScreen = document.querySelector(".hello-screen");
     helloScreen.classList.add("hide");
+    unhideMainPage();
+    
 }
 
-export function unhideMainPage() {
+function unhideMainPage() {
     const mainPage = document.querySelector(".main");
     mainPage.classList.remove("hide");
+    const knowMoreButton = document.querySelector("#home-page a");
+    knowMoreButton.addEventListener("animationend", () => {
+        setTimeout(animateDesignation, 1500);
+    }, { once: true });
 }
 
-export function animateDesignation() {
+function animateDesignation(index = 1, key = null) {
     const designation = document.querySelector("#home-page .designation");
-    const key = setInterval(() => {
-        designation.classList.add("fade-out");
-        setTimeout(() => {
-            designation.classList.remove("fade-out");
-            if (designation.textContent == "I am a Full Stack    Developer") {
-                designation.textContent = "I build cool websites";
-            }
-            else if (designation.textContent == "I build cool websites")
-                designation.textContent = "I am a Front End Developer";
-            else    
-                designation.textContent = "I am a Full Stack    Developer";
-            designation.classList.add("fade-in");
-         }, 500);
-    },3000)
+    designation.classList.remove("fade-in");
+    designation.classList.add("fade-out");
+    const textArray = [
+        "I build cool websites",
+        "I am a Full Stack Developer",
+        "I am a Front End Developer"
+    ];
+    if (key !== null) {
+        clearTimeout(key);
+    }
+    
+    designation.addEventListener("animationend", () => {
+        designation.classList.remove("fade-out");
+        designation.textContent = textArray[index];
+        designation.classList.add("fade-in");
+        index++;
+        if (index === textArray.length) {
+            index = 0;
+        }
+        setTimeout(()=>0)
+        key = setTimeout(() => animateDesignation(index, key),2000);
+    },{once:true})
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import About from './about'
 import './index.css'
-import { hideLoading,startHelloScreen,hideHelloScreen,unhideMainPage,animateDesignation } from './helpers'
+import {startHelloScreen,hideHelloScreen} from './helpers'
 
 ReactDOM.createRoot(document.getElementById('about-page')).render(
   <React.StrictMode>
@@ -11,12 +11,88 @@ ReactDOM.createRoot(document.getElementById('about-page')).render(
 )
 
 window.onload = () => {
-  // setTimeout(hideLoading,250);
-  setTimeout(startHelloScreen, 500);
-  setTimeout(hideHelloScreen, 3520);
-  setTimeout(unhideMainPage, 3540);
-  setTimeout(animateDesignation, 6040);
+  const helloScreen = document.querySelector(".hello-screen")
+  setTimeout(startHelloScreen, 1000);
+  helloScreen.addEventListener("animationend", hideHelloScreen);
 }
 
+// eslint-disable-next-line no-unused-vars
+const initializer = (function () {
+  let isDark = false;
+  const sections = document.querySelectorAll(".page-section");
+  const navs = document.querySelectorAll("#nav-bar a");
 
-                
+  window.onscroll = () => {
+    //this part sets the nav bar icon to active according to the position in page
+    let current = "";
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      if (scrollY >= sectionTop - 200) {
+        current = section.getAttribute("id");
+      }
+    });
+    let checkId = current + "-link"
+    navs.forEach((nav) => {
+      nav.classList.remove("active");
+      if (nav.id === checkId) {
+        nav.classList.add("active");
+      }
+    });
+    //This part changes the color scheme of webpage when scrolling down
+    const remainingPage = document.querySelector("#remaining-page");
+    const remainingPageTop = remainingPage.offsetTop;
+    if (scrollY >= remainingPageTop - 450 && !isDark) {
+      isDark = !isDark;
+      changeColorScheme(isDark);
+    }
+    else if (scrollY < remainingPageTop - 450 && isDark) {
+      isDark = !isDark;
+      changeColorScheme(isDark)
+    }
+
+  }
+})();
+
+// function changeColorScheme(isDark) {
+//   let root = document.querySelector(":root");
+//   let colorLight, colorEnd, colorMedium, colorDark, colorNeon;
+//   if (isDark) {
+//     colorLight = "#0b192e";
+//     colorEnd = "#000000";
+//     colorMedium = "#4e5258";
+//     colorDark = "#cedcf5";
+//     colorNeon = "#05bfdb";
+//   }
+//   else {
+//     colorLight = "#cedcf5";
+//     colorEnd = "#ffffff";
+//     colorMedium = "#4e5258";
+//     colorDark = "#0b192e";
+//     colorNeon = "#05bfdb";
+//   }
+
+//   root.classList.add("quick-fade-out");
+//   root.addEventListener("animationend", () => {
+//     root.style.setProperty('--color-light', colorLight);
+//     root.style.setProperty('--color-end', colorEnd);
+//     root.style.setProperty('--color-medium', colorMedium);
+//     root.style.setProperty('--color-dark', colorDark);
+//     root.style.setProperty('--color-neon', colorNeon);
+//     root.classList.remove("quick-fade-out");
+//     root.classList.add("quick-fade-in");
+//     root.addEventListener("animationend", () => root.classList.remove("quick-fade-in"));
+//   });
+// }
+  function changeColorScheme(isDark) {
+    let root = document.querySelector(":root");
+    if (isDark) {
+      root.classList.remove("light");
+      root.classList.add("dark");
+    }
+    else {
+      root.classList.remove("dark");
+      root.classList.add("light");      
+    }
+  }
+
+
