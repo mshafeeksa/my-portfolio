2e9f6890e1415cc0b96b40edcfa5a7ea14289858
Mohammed Shafeek <mail2shafeek.sa@gmai.com>
2024-01-07 21:23:08 +0400

    Remove type attribute from object

--- src/Avatar.jsx
+++ src/Avatar.jsx
@@ -1,6 +1,6 @@
 import Avatar from "./assets/avatar.svg"
 export default function InsertAvatar() {
     return (
-        <object type="image/svg+xml" data={Avatar}></object> 
+        <object data={Avatar}></object> 
     );
 }
